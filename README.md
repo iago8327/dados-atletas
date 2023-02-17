a42f9fb842f65960906a5cfba401b2f216807109
iago8327 <iago.p.silva.8327@gmail.com>
2023-02-16 21:25:52 -0300

    commit final

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# dados-atletas
-DEVstart / Projeto de certificação 2 - Dados dos atletas
+## DEVstart / Projeto de certificação 2 – Dados dos atletas
+
+#### Descrição
+Este é o segundo projeto de certificação exigido pelo curso de programação DEVstart, projeto esse feito em javascript.
+
+#### Objetivo
+Tal projeto deveria receber algumas informações de um determinado atleta (nome, idade, peso, altura e notas) e deveria retornar a categoria em qual o mesmo se encaixava, seu IMC, a média feita com suas três notas principais e as demais informações mensionadas anteriormente.
--- dados-atletas.js
+++ dados-atletas.js
@@ -1,77 +1,82 @@
-class Atletas () {
-    construtor(nome, idade, peso, altura, notas) {
-        this.nome = nome;
-        this.idade = idade;
-        this.peso = peso;
-        this.altura = altura;
-        this.notas = notas;
-    }
-
-    calculaCategoria() {
-        if (this.idade >= 9 && this.idade <= 11){
-            return `Infantil`;
-        }
-
-        if (this.idade >= 12 && this.idade <= 13){
-            return `Juvenil`;
-        }
-
-        if (this.idade >= 14 && this.idade <= 15){
-            return `Intermediário`;
-        }
-
-        if (this.idade >= 16 && this.idade <= 30){
-            return `Adulto`;
-        }
-
-        if (this.idade < 9 || this.idade > 30){
-            return `Sem categoria`;
-        }
-    }
-
-    calculaIMC() {
-        let imc = this.altura * this.altura / this.peso;
-    }
-
-    calculaMediaValida() {
-        let novasNotas = this.notas.sort((a,b) => a - b).slice(1,4);
-        let notaBase = 0;
-        for (let i = 0; i < novasNotas.length; i++) {
-            notaBase += novasNotas[i];
-        }
-        this.notaBase / this.novasNotas.length;
-    }
-
+class Atleta {
+    constructor (nome, idade, peso, altura, notas){
+      this.nome = nome
+      this.idade = idade
+      this.peso = peso
+      this.altura = altura
+      this.notas = notas
+      }
+  
+    calculaCategoria(){
+      if (this.idade >= 9 && this.idade < 12){
+                  return `Infantil`;
+              }
+      
+      if (this.idade >= 12 && this.idade < 14){
+                  return `Juvenil`;
+              }
+      
+      if (this.idade >= 14 && this.idade < 16){
+                  return `Intermediário`;
+              }
+      
+      if (this.idade >= 16 && this.idade < 31){
+                  return `Adulto`;
+              }
+      
+      if (this.idade < 9 || this.idade > 30){
+                  return `Sem categoria`;
+              }
+      }
+  
+    calculaIMC(){
+      return this.peso / (this.altura * this.altura);
+      }
+  
+    calculaMediaValida(){
+      let novasNotas = this.notas.sort((a,b) => a - b).slice(1,4);
+      let notaBase = 0;
+      notaBase = novasNotas.reduce(function (total, atual){
+        return total + atual;
+          });
+      return notaBase / novasNotas.length;
+              
+      }
+  
     obtemNomeAtleta() {
-        console.log(this.nome);
-    }
-
+      return console.log(`Nome: ${this.nome}`);
+          }
+      
     obtemIdadeAtleta() {
-        console.log(this.idade);
-    }
-
+      return console.log(`Idade: ${this.idade}`);
+          }
+      
     obtemPesoAtleta() {
-        console.log(this.peso);
-    }
-
+      return console.log(`Peso: ${this.peso}`);
+          }
+      
     obtemNotasAtleta() {
-        console.log(this.notas);
-    }
-
-    obtemCategoria() {
-        console.log(this.calculaCategoria())
-    }
-
-    obtemIMC() {
-        console.log(this.caculaIMC());
-    }
-
-    obtemMediaValida() {
-        console.log(this.calculaMediaValida());
-    }
-}
-
-
-const atleta = new Atleta("Cesar Abascal",
-    30, 80, 1.70,
-    [10, 9.34, 8.42, 10, 7.88]);
+      return console.log(`Notas: ${this.notas}`);
+          }
+  
+    obtemCategoria(){
+      return console.log(`Categoria: ${this.calculaCategoria()}`)
+      }
+  
+    obtemIMC(){
+      return console.log(`IMC: ${this.calculaIMC()}`);
+      }
+  
+    obtemMediaValida(){
+      return console.log(`Média válida: ${this.calculaMediaValida()}`);
+      }
+  }
+  
+  const atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88])
+  atleta.obtemNomeAtleta();
+  atleta.obtemIdadeAtleta();
+  atleta.obtemPesoAtleta();
+  atleta.obtemNotasAtleta();
+  atleta.obtemCategoria();
+  atleta.obtemIMC();
+  atleta.obtemMediaValida();
